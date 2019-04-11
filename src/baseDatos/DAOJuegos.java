88f793d33e1b4d421869dc99e39e5ff3ea7f3aa1
alumnogreibd <alumnogreibd@alumnogreibd-VirtualBox>
2019-04-11 12:02:50 +0200

    Pablo Dao Juegos y juego, desarrolladora-> leer comentario de desarrolladora

--- src/baseDatos/DAOJuegos.java
+++ src/baseDatos/DAOJuegos.java
@@ -6,7 +6,8 @@
 package baseDatos;
 
 import java.sql.*;
-
+import aplicacion.Juego;
+import aplicacion.Desarrolladora;
 /**
  *
  * @author Alex
@@ -16,6 +17,40 @@ public class DAOJuegos  extends AbstractDAO{
     public DAOJuegos(Connection con, aplicacion.FachadaAplicacion fa){
         super.setConexion(con);
         super.setFachadaAplicacion(fa);
+    }
+    
+    public java.util.List<Juego> consultarJuegosTienda(String categoria, String desarrolladora, String nombre){
+        
+        java.util.List<Juego> resultado = new java.util.ArrayList<Juego>();
+        Juego jactual;
+        PreparedStatement stmc = null;
+        ResultSet rst;
+        Connection con;
+        
+        con=this.getConexion();
+        try{
+        stmc= con.prepareStatement("select j.id, j.nombre, j.edadrecomendada, j.desarrolladora, j.pais"
+        +"from Juego as j, TenerCategoria as c, Desarrolladora as d"
+        +"where j.nombre like c.juego "
+        +"and j.desarrolladora like d.nombre "
+        +"and j.nombre like %?% "
+        +"and j.desarrolladora like ? "
+        +"and c.categoria like ?");
+        stmc.setString(1, nombre);
+        stmc.setString(2, desarrolladora);
+        stmc.setString(3, categoria);
+        
+        rst=stmc.executeQuery();
+        while(rst.next()){
+            jactual = new Juego(rst.getInt("j.id"), rst.getString("j.nombre"), rst.getInt("j.edadrecomendada"), null, new Desarrolladora(rst.getString("desarrolladora"), rst.getString("pais")));
+        }
+        
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraAvisoCorrecto("Error al buscar en la tienda");
+        }
+        return resultado;
     }
 }
 
