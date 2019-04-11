de1c7760a7973676e5f1be6bc545b9f171d27d23
gonzalezcostamanuel <gonzalezcostamanuel@gmail.com>
2019-04-11 11:09:22 +0200

    creada

---
+++ src/gui/ModeloTablaJugador.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui;
+import aplicacion.Jugador;
+import javax.swing.table.*;
+/**
+ *
+ * @author basesdatos
+ */
+public class ModeloTablaJugador extends AbstractTableModel{
+    private java.util.List<Jugador> jugadores;
+
+    public ModeloTablaJugador(){
+        this.jugadores=new java.util.ArrayList<>();
+    }
+
+    @Override
+    public int getColumnCount (){
+        return 1;
+    }
+
+    @Override
+    public int getRowCount(){
+        return jugadores.size();
+    }
+
+    @Override
+    public String getColumnName(int col){
+        String nombre="";
+
+        switch (col){
+            case 0: nombre= "Nombre"; break;
+        }
+        return nombre;
+    }
+
+    @Override
+    public Class getColumnClass(int col){
+        Class clase=null;
+
+        switch (col){
+            case 0: clase= java.lang.String.class; break;
+        }
+        return clase;
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int col){
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col){
+        Object resultado=null;
+
+        switch (col){
+            case 0: resultado= jugadores.get(row).getLogin(); break;
+        }
+        return resultado;
+    }
+
+    public void setFilas(java.util.List<Jugador> jugadores){
+        this.jugadores = jugadores;
+        fireTableDataChanged();
+    }
+
+    public Jugador obtenerJugador(int i){
+        return this.jugadores.get(i);
+    }
+
+}
