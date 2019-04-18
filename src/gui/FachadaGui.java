a144344cbf460db701a5925047622b29cf0d8a16
ZnGAlex <37783776+ZnGAlex@users.noreply.github.com>
2019-04-18 11:44:37 +0200

    Arreglo doble apertura de ventanas

--- src/gui/FachadaGui.java
+++ src/gui/FachadaGui.java
@@ -24,49 +24,42 @@ public class FachadaGui {
         VLogin va;
 
         va = new VLogin(this.vp, true, fa);
-        va.setVisible(true);
     }
 
     public void muestraAvisoCorrecto(String s) {
         VAvisoCorrecto vac;
 
         vac = new VAvisoCorrecto(vp, true, s);
-        vac.setVisible(true);
     }
 
     public void muestraVVerDetalles(Juego juego) {
         VVerDetalles vvd;
 
         vvd = new VVerDetalles(this.vp, true, fa, juego);
-        vvd.setVisible(true);
     }
 
     public void muestraVMiPerfil(Usuario usuario) {
         VMiPerfil vmp;
 
         vmp = new VMiPerfil(this.vp, true, false, usuario);
-        vmp.setVisible(true);
     }
     
     public void muestraVMisAmigos(Usuario usuario){
         VMisAmigos vma;
         
         vma = new VMisAmigos(this.vp,true,fa,usuario);
-        vma.setVisible(true);
     }
     
     public void muestraVMisJuegos(Usuario usuario){
         VMisJuegos vmj;
         
         vmj = new VMisJuegos(this.vp,true,fa,usuario);
-        vmj.setVisible(true);
     }
     
     public void muestraVCarrito(Usuario usuario, java.util.List<Juego> juegos){
         VCarrito vc;
         
         vc = new VCarrito(this.vp,true,fa,usuario,juegos);
-        vc.setVisible(true);
     }
 
 }
